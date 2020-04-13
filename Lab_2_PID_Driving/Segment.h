578472d3fe621f53a01751a5e33d27d4cc72b254
salvattos <swsalvatto@wpi.edu>
2020-04-13 18:06:48 -0400

    Update Segment.h

--- Lab_2_PID_Driving/Segment.h
+++ Lab_2_PID_Driving/Segment.h
@@ -15,7 +15,7 @@ int countLimit;
 
 const int numberOfSegments = 3;
 Segment seg1 = {30,30,55};
-Segment seg2 = {0,30,15};
+Segment seg2 = {0,50,15};
 Segment seg3 = {30,30,35};
 
 Segment segments[numberOfSegments] = {seg1,seg2,seg3};
